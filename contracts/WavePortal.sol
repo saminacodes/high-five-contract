5a56464fe3fdacd48cfc6196abb83f30d6521c86
Samina <saminacodes@icloud.com>
2022-05-18 13:08:17 -0600

    removed console logs

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -2,22 +2,17 @@
 
 pragma solidity ^0.8.0;
 
-import "hardhat/console.sol";
-
 contract WavePortal {
     uint256 totalWaves;
 
     constructor() {
-        console.log("Hello, World!");
     }
 
     function wave() public {
         totalWaves += 1;
-        console.log("%s has waved!", msg.sender);
     }
 
     function getTotalWaves() public view returns (uint256) {
-        console.log("We have %d total waves!", totalWaves);
         return totalWaves;
     }
 }
